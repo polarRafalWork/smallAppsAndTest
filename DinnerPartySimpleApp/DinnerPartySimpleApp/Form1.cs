2c19e83785061a1679ccdc16999ea614969dd5a5
polar.rafal.work <polar.rafal.work@gmail.com>
2018-04-11 10:40:22 +0200

    StartingProject

---
+++ DinnerPartySimpleApp/DinnerPartySimpleApp/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DinnerPartySimpleApp
+{
+    public partial class Form_DinnerPartyPlanner : Form
+    {
+        public Form_DinnerPartyPlanner()
+        {
+            InitializeComponent();
+        }
+    }
+}
